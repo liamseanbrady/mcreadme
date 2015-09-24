9335bdb25b1d04f8fd9274bba54c4c6eca77f401
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-25 00:36:59 +0100

    Move creation of file to FileMaker

--- lib/mcreadme/read_me.rb
+++ lib/mcreadme/read_me.rb
@@ -1,9 +1,24 @@
 module Mcreadme
   class ReadMe
+    attr_reader :file_maker
+
     def self.create(path)
-      File.open(path, 'a') do |file|
-        file << 'A README'
-      end
+      read_me = new(path)
+      content = read_me.basic
+      read_me.file_maker.make(content, path) 
     end
+
+    def initialize(path, file_maker = FileMaker)
+      @path = path
+      @file_maker = file_maker
+    end
+
+    def basic
+      "# Title\n"
+    end
+
+    private
+      
+    attr_reader :path
   end
 end
