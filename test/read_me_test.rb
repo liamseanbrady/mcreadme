bca8784a82294f6adaf89e32ab1e6837feb43482
Liam Sean Brady <liamseanbrady@gmail.com>
2015-09-25 15:00:53 +0100

    Test that README will be added to current directory

--- test/read_me_test.rb
+++ test/read_me_test.rb
@@ -3,12 +3,7 @@ require_relative 'test_helper'
 
 class ReadMeTest < Minitest::Test
   def test_populates_a_basic_read_me
-    file_maker = Class.new(Mcreadme::FileMaker) do
-      def self.make(content, path)
-      end
-    end
-
-    read_me = Mcreadme::ReadMe.new('/tmp/basic_readme', file_maker)
+    read_me = Mcreadme::ReadMe.new(path: '/tmp/basic_readme')
     basic_readme_content = read_me.basic
     basic_readme_fixture = File.read(File.expand_path('../fixtures/basic_readme', __FILE__))
 
@@ -16,12 +11,15 @@ class ReadMeTest < Minitest::Test
   end
 
   def test_creates_a_readme_in_current_working_directory
-    file = Class.new(File) do
-      def open(permission, path)
-        assert_equal(Dir.pwd, path) 
+    env = self
+    file_maker = Class.new(Mcreadme::FileMaker) do
+      @minitest = env
+      def self.make(content, path)
+        expected_path = File.expand_path('README.md', File.expand_path(Dir.pwd))
+        @minitest.send(:assert_equal, expected_path, path)
       end
     end
 
-    Mcreadme::ReadMe.create
+    Mcreadme::ReadMe.create(file_maker: file_maker)
   end
 end
